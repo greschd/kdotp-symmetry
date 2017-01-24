e848062c095d5b3008bb92741500b060e04e2708
Dominik Gresch <greschd@gmx.ch>
2017-01-24 13:23:21 +0100

    added test for raising ValueError when inconsistent dim of bases is passed

--- tests/test_linalg.py
+++ tests/test_linalg.py
@@ -29,6 +29,24 @@ from kdotp_symmetry.linalg import zassenhaus, intersection_basis
 def test_zassenhaus(input_bases, output_bases):
     assert zassenhaus(*input_bases) == output_bases
 
+@pytest.mark.parametrize('input_bases', [
+    (
+        [[0, 1, 0], [0, 1, 1]],
+        [[0, 1]],
+    ),
+    (
+        [[0, 1], [0, 1, 1]],
+        [[0, 1, 0]],
+    ),
+    (
+        [[0, 1, 0], [0, 1]],
+        [[0, 1, 0]],
+    ),
+])
+def test_zassenhaus_inconsistent_dim(input_bases):
+    with pytest.raises(ValueError):
+            zassenhaus(*input_bases)
+
 @pytest.mark.parametrize('input_bases,output_basis', [
     (
         ([[1, 1, 0], [0, 1, 0]], [[0, 1, 1], [0, 0, 1]], [[0, 2, 1]], []),
