bdc27302d371649517343336a5a15a7b03c1c2b6
Dominik Gresch <greschd@gmx.ch>
2017-01-24 13:19:16 +0100

    linting linalg

--- kdotp_symmetry/linalg.py
+++ kdotp_symmetry/linalg.py
@@ -19,9 +19,9 @@ def zassenhaus(basis_a, basis_b):
     # handle the case where one of the bases is empty
     if len(basis_a) == 0 or len(basis_b) == 0:
         mat, pivot = sp.Matrix(basis_a).rref()
-        plus_basis = mat[:len(pivot),:].tolist()
+        plus_basis = mat[:len(pivot), :].tolist()
         return ZassenhausResult(sum=plus_basis, intersection=[])
-    
+
     else:
         # Set up the Zassenhaus matrix from the given bases.
         A = sp.Matrix(basis_a)
@@ -31,22 +31,22 @@ def zassenhaus(basis_a, basis_b):
             raise ValueError('Inconsistent dimensions of the two bases given.')
         zassenhaus_mat = A.row_join(A).col_join(B.row_join(sp.zeros(*B.shape)))
         mat, pivot = zassenhaus_mat.rref()
-        
+
         # idx is the row index of the first row belonging to the intersection basis
         idx = np.searchsorted(pivot, dim)
         plus_basis = mat[:idx, :dim].tolist()
         # The length of the pivot table is used to get rid of all-zero rows
         int_basis = mat[idx:len(pivot), dim:].tolist()
         return ZassenhausResult(sum=plus_basis, intersection=int_basis)
-    
+
 def intersection_basis(*bases):
-    bases_sorted = sorted(bases, key=lambda x: len(x))
+    r"""
+    Given ``bases`` of different subspaces :math:`U_i \subseteq V`, returns a basis of the intersection :math:`\bigcap_i U_i`. The basis vectors must all have the same length.
+    """
+    bases_sorted = sorted(bases, key=len)
     current_basis = bases_sorted.pop(0)
-    for b in bases_sorted:
+    for basis in bases_sorted:
         if len(current_basis) == 0:
             break
-        current_basis = zassenhaus(current_basis, b).intersection
+        current_basis = zassenhaus(current_basis, basis).intersection
     return current_basis
-    
-if __name__ == '__main__':
-    print(intersection_basis([[1, 1, 0], [0, 1, 0]], [[0, 1, 1], [0, 0, 1]], [[0, 2, 1]], []))
