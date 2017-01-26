18ea988d1e8efe6235ba2fed5b3c1147b7374273
Dominik Gresch <greschd@gmx.ch>
2017-01-26 11:58:54 +0100

    made hermitian basis non-normalized to make result cleaner

--- kdotp_symmetry/repr_utils.py
+++ kdotp_symmetry/repr_utils.py
@@ -15,7 +15,7 @@ def frobenius_product(A, B):
 
 def hermitian_basis(dim):
     """
-    Returns an orthonormal basis (w.r.t. the Frobenius scalar product) of the hermitian matrices of size 'dim'.
+    Returns an orthogonal basis (w.r.t. the Frobenius scalar product) of the hermitian matrices of size 'dim'.
     """
     basis = []
     # diagonal entries
@@ -25,33 +25,32 @@ def hermitian_basis(dim):
         basis.append(mat)
 
     # off-diagonal entries
-    x = 1 / sp.sqrt(2)
     for i in range(dim):
         for j in range(i + 1, dim):
             # real
             mat = sp.zeros(dim)
-            mat[i, j] = x
-            mat[j, i] = x
+            mat[i, j] = 1
+            mat[j, i] = 1
             basis.append(mat)
 
             # imag
             mat = sp.zeros(dim)
-            mat[i, j] = -1j * x
-            mat[j, i] = 1j * x
+            mat[i, j] = -sp.numbers.I
+            mat[j, i] = sp.numbers.I
             basis.append(mat)
 
     # check ONB property
     assert len(basis) == dim**2
-    _assert_onb(basis)
+    _assert_orthogonal(basis)
 
     return basis
 
-def _assert_onb(basis):
+def _assert_orthogonal(basis):
     """Check ONB properties for a given ``basis``."""
     for i, bi in enumerate(basis):
         for j, bj in enumerate(basis):
             if i == j:
-                assert frobenius_product(bi, bj) == 1
+                assert frobenius_product(bi, bj) != 0
             else:
                 assert frobenius_product(bi, bj) == 0
 
@@ -59,8 +58,8 @@ def hermitian_to_vector(matrix, basis):
     """
     Returns a the vector representing the ``matrix`` w.r.t. the given *orthonormal* ``basis``.
     """
-    _assert_onb(basis)
-    vec = tuple(frobenius_product(matrix, b) for b in basis)
+    _assert_orthogonal(basis)
+    vec = tuple(frobenius_product(matrix, b) / frobenius_product(b, b) for b in basis)
     # check consistency
     assert sp.Eq(sum((v * b for v, b in zip(vec, basis)), sp.zeros(*matrix.shape)), matrix)
     return vec
