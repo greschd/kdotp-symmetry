2c89d1afdda67d65dafeac486686553ca654765f
Dominik Gresch <greschd@gmx.ch>
2017-01-28 01:21:01 +0100

    changed tests to reflect only returning the final basis

--- tests/test_symmetric_hamiltonian.py
+++ tests/test_symmetric_hamiltonian.py
@@ -25,19 +25,10 @@ kx, ky, kz = sp.symbols('kx, ky, kz')
         )],
         kp.monomial_basis(0),
         'auto',
-        (
-            [[1, 1, 0, 0], [0, 0, 1, 0]], 
-            [
-                Matrix([[1, 0], [0, 0]]), 
-                Matrix([[0, 0], [0, 1]]), 
-                Matrix([[0, 1], [1, 0]]), 
-                Matrix([[0, -I], [I,  0]])
-            ], 
-            [
-                Matrix([[1, 0], [0, 1]]), 
-                Matrix([[0, 1], [1, 0]])
-            ]
-        )
+        [
+            Matrix([[1, 0], [0, 1]]), 
+            Matrix([[0, 1], [1, 0]])
+        ]
     ),
     (
         [kp.SymmetryOperation(
@@ -49,31 +40,14 @@ kx, ky, kz = sp.symbols('kx, ky, kz')
         )],
         kp.monomial_basis(1),
         'auto',
-        (
-            [[0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0], [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0], [0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0], [0, 0, 0, -1, 0, 0, 0, 1, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0]], 
-            [
-                Matrix([[kx, 0],[ 0, 0]]),
-                Matrix([[0,  0],[0, kx]]),
-                Matrix([[0, kx],[kx,  0]]),
-                Matrix([[0, -I*kx],[I*kx, 0]]),
-                Matrix([[ky, 0],[0, 0]]),
-                Matrix([[0,  0],[0, ky]]),
-                Matrix([[0, ky],[ky, 0]]),
-                Matrix([[0, -I*ky],[I*ky, 0]]),
-                Matrix([[kz, 0],[0, 0]]),
-                Matrix([[0,  0],[0, kz]]),
-                Matrix([[0, kz],[kz, 0]]),
-                Matrix([[0, -I*kz],[I*kz, 0]])
-            ], 
-            [
-                Matrix([[ky, 0],[0, kx]]),
-                Matrix([[kx,  0],[0, ky]]),
-                Matrix([[0, kx + ky],[kx + ky, 0]]),
-                Matrix([[0, I*kx - I*ky],[-I*kx + I*ky, 0]]),
-                Matrix([[kz,  0],[ 0, kz]]),
-                Matrix([[ 0, kz],[kz,  0]])
-            ]
-        )
+        [
+            Matrix([[ky, 0],[0, kx]]),
+            Matrix([[kx,  0],[0, ky]]),
+            Matrix([[0, kx + ky],[kx + ky, 0]]),
+            Matrix([[0, I*kx - I*ky],[-I*kx + I*ky, 0]]),
+            Matrix([[kz,  0],[ 0, kz]]),
+            Matrix([[ 0, kz],[kz,  0]])
+        ]
     ),
 ])
 def test_symmetric_hamiltonian(symmetry_operations, expr_basis, repr_basis, result):
@@ -83,47 +57,3 @@ def test_symmetric_hamiltonian(symmetry_operations, expr_basis, repr_basis, resu
         repr_basis=repr_basis
     ) == result
 
-@pytest.mark.parametrize('symmetry_operations,expr_basis,repr_basis', [
-    (
-        [kp.SymmetryOperation(
-            kmatrix=[[0, 1, 0], [1, 0, 0], [0, 0, 1]],
-            repr=kp.Representation(
-                matrix=[[0, 1], [1, 0]],
-                complex_conjugate=False
-            )
-        )],
-        kp.monomial_basis(0),
-        'auto'
-    ),
-    (
-        [kp.SymmetryOperation(
-            kmatrix=[[0, 1, 0], [1, 0, 0], [0, 0, 1]],
-            repr=kp.Representation(
-                matrix=[[0, 1], [1, 0]],
-                complex_conjugate=False
-            )
-        )],
-        kp.monomial_basis(1),
-        'auto'
-    ),
-    (
-        [kp.SymmetryOperation(
-            kmatrix=[[0, 1, 0], [1, 0, 0], [0, 0, 1]],
-            repr=kp.Representation(
-                matrix=[[0, 1], [1, 0]],
-                complex_conjugate=False
-            )
-        )],
-        kp.monomial_basis(*range(3)),
-        'auto'
-    ),
-])
-def test_symmetric_hamiltonian_consistency(symmetry_operations, expr_basis, repr_basis, compare_equal):
-    compare_equal(sp.lambdify(
-        (), 
-        kp.symmetric_hamiltonian(
-            *symmetry_operations, 
-            expr_basis=expr_basis, 
-            repr_basis=repr_basis
-        )[0]
-    )())
