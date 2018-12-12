6ccb6274a91c03fa5d53a8b4b66905192a96113b
Dominik Gresch <greschd@gmx.ch>
2018-12-12 18:30:56 +0100

    Change to_matrix to use keywords.

--- tests/test_to_matrix.py
+++ tests/test_to_matrix.py
@@ -14,72 +14,82 @@ from kdotp_symmetry._repr_utils import hermitian_to_vector, hermitian_basis, rep
 
 
 @pytest.mark.parametrize(
-    'operator,basis,to_vector_fct,result', [(
-        matrix_to_expr_operator([[0, 1, 0], [1, 0, 0], [0, 0, -1]]),
-        monomial_basis(*range(3)), expr_to_vector,
-        sp.Matrix([
-            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
-            [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, -1, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
-            [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, -1, 0],
-            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, -1, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        ])
-    ), (
-        matrix_to_expr_operator([[0, 1, 0], [1, 0, 0], [0, 0, -1]]),
-        monomial_basis(*range(2)), expr_to_vector,
-        sp.Matrix([[1, 0, 0, 0], [0, 0, 1, 0], [0, 1, 0, 0], [0, 0, 0, -1]])
-    ), (
-        repr_to_matrix_operator(sp.Matrix([[0, 1], [1, 0]])),
-        hermitian_basis(2), hermitian_to_vector,
-        sp.Matrix([
-            [0, 1, 0, 0],
-            [1, 0, 0, 0],
-            [0, 0, 1, 0],
-            [0, 0, 0, -1],
-        ])
-    ), (
-        repr_to_matrix_operator(
-            sp.Matrix([[0, 1], [1, 0]]), complex_conjugate=True
-        ), hermitian_basis(2), hermitian_to_vector,
-        sp.Matrix([
-            [0, 1, 0, 0],
-            [1, 0, 0, 0],
-            [0, 0, 1, 0],
-            [0, 0, 0, 1],
-        ])
-    ), (
-        repr_to_matrix_operator(sp.eye(2), complex_conjugate=True),
-        hermitian_basis(2), hermitian_to_vector,
-        sp.Matrix([
-            [1, 0, 0, 0],
-            [0, 1, 0, 0],
-            [0, 0, 1, 0],
-            [0, 0, 0, -1],
-        ])
-    ), (
-        repr_to_matrix_operator(
-            sp.diag(
-                sp.exp(-sp.I * sp.pi * sp.Rational(2, 3)),
-                sp.exp(sp.I * sp.pi * sp.Rational(2, 3))
-            ),
-            complex_conjugate=False
-        ), hermitian_basis(2), hermitian_to_vector,
-        sp.Matrix([
-            [1, 0, 0, 0],
-            [0, 1, 0, 0],
-            [0, 0, -sp.Rational(1, 2),
-             sp.sqrt(3) / 2],
-            [0, 0, -sp.sqrt(3) / 2, -sp.Rational(1, 2)],
-        ])
-    )]
+    'operator,basis,to_vector_fct,result', [
+        (
+            matrix_to_expr_operator([[0, 1, 0], [1, 0, 0], [0, 0, -1]]),
+            monomial_basis(*range(3)), expr_to_vector,
+            sp.Matrix([
+                [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+                [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, -1, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+                [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, -1, 0],
+                [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, -1, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            ])
+        ),
+        (
+            matrix_to_expr_operator([[0, 1, 0], [1, 0, 0], [0, 0, -1]]),
+            monomial_basis(*range(2)), expr_to_vector,
+            sp.Matrix([[1, 0, 0, 0], [0, 0, 1, 0], [0, 1, 0, 0], [0, 0, 0, -1]]
+                      )
+        ),
+        (
+            repr_to_matrix_operator(sp.Matrix([[0, 1], [1, 0]])),
+            hermitian_basis(2), hermitian_to_vector,
+            sp.Matrix([
+                [0, 1, 0, 0],
+                [1, 0, 0, 0],
+                [0, 0, 1, 0],
+                [0, 0, 0, -1],
+            ])
+        ),
+        (
+            repr_to_matrix_operator(
+                sp.Matrix([[0, 1], [1, 0]]), complex_conjugate=True
+            ), hermitian_basis(2), hermitian_to_vector,
+            sp.Matrix([
+                [0, 1, 0, 0],
+                [1, 0, 0, 0],
+                [0, 0, 1, 0],
+                [0, 0, 0, 1],
+            ])
+        ),
+        (
+            repr_to_matrix_operator(sp.eye(2), complex_conjugate=True),
+            hermitian_basis(2), hermitian_to_vector,
+            sp.Matrix([
+                [1, 0, 0, 0],
+                [0, 1, 0, 0],
+                [0, 0, 1, 0],
+                [0, 0, 0, -1],
+            ])
+        ),
+        (
+            repr_to_matrix_operator(
+                sp.diag(
+                    sp.exp(-sp.I * sp.pi * sp.Rational(2, 3)),
+                    sp.exp(sp.I * sp.pi * sp.Rational(2, 3))
+                ),
+                complex_conjugate=False
+            ), hermitian_basis(2), hermitian_to_vector,
+            sp.Matrix([
+                [1, 0, 0, 0],
+                [0, 1, 0, 0],
+                [0, 0, -sp.Rational(1, 2),
+                 sp.sqrt(3) / 2],
+                [0, 0, -sp.sqrt(3) / 2, -sp.Rational(1, 2)],
+            ])
+        )
+    ]
 )
 def test_to_matrix(operator, basis, to_vector_fct, result):
     """
     Test that turning a matrix / expression into matrix form gives the desired result.
     """
-    assert to_matrix(operator, basis, to_vector_fct) == result
+    assert to_matrix(
+        operator=operator, basis=basis, to_vector_fct=to_vector_fct
+    ) == result
